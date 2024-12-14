4ae542b9a5760f9cc8373236d6d582bce7e93e73
Wakchaure123 <133526230+Wakchaure123@users.noreply.github.com>
2024-12-14 12:32:22 +0530

    if else

---
+++ src/SimleProgramme/ifelse.java
@@ -0,0 +1,19 @@
+package SimleProgramme;
+
+public class ifelse {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		boolean learning  = true;
+		
+		if(learning) {
+			System.out.println("I am learning");
+			
+		}
+		else System.out.println("i am not learnig");
+		
+	}
+
+}
