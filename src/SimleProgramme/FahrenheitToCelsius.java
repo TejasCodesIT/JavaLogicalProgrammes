9b42707ef1e033c34b83ac7afa9d58cee4c08617
Wakchaure123 <133526230+Wakchaure123@users.noreply.github.com>
2024-12-14 12:02:54 +0530

    Farhenieght

---
+++ src/SimleProgramme/FahrenheitToCelsius.java
@@ -0,0 +1,31 @@
+package SimleProgramme;
+
+import java.util.Scanner;
+
+public class FahrenheitToCelsius {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		float temperature ;
+		
+		Scanner sc= new Scanner(System.in);
+		
+		
+		System.out.println("Enter temperature in fahrenheit");
+		
+		temperature=  sc.nextFloat();
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
---
+++ src/SimleProgramme/Scannerexample.java
@@ -0,0 +1,27 @@
+package SimleProgramme;
+
+import java.util.Scanner;
+
+public class Scannerexample {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		System.out.println("Enter NAME  ");
+		String name = sc.next() ;
+		
+		System.out.println("Enter Last Name");
+		String lastname=sc.next();
+		
+		
+		System.out.println(name+"  "+lastname);
+		
+	}
+
+}
