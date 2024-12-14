333ab22085702810f0a880a95c54ac031b6af138
Wakchaure123 <133526230+Wakchaure123@users.noreply.github.com>
2024-12-14 11:53:02 +0530

    Commandline arguments

---
+++ src/SimleProgramme/CommanfLineArgument.java
@@ -0,0 +1,22 @@
+package SimleProgramme;
+
+public class CommanfLineArgument {
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		for(String t : args) {
+			
+			System.out.println(t);
+			
+		}
+		
+	}
+	
+
+}
