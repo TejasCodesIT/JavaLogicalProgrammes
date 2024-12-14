0f57459248c5aec4e86430e948f6d887b20a50a9
Wakchaure123 <133526230+Wakchaure123@users.noreply.github.com>
2024-12-14 11:48:07 +0530

    Integer

---
+++ src/SimleProgramme/Integer.java
@@ -0,0 +1,22 @@
+package SimleProgramme;
+
+public class Integer {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		int c;
+		
+		
+		for(c=1; c<=100 ; c++ ) {
+			
+			System.out.println(c);
+		}
+		
+	}
+	
+	
+
+}
